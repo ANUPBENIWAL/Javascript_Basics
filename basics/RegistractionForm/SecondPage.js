1f1e860de5a386481380417e130b7f5255758a02
Anup <anupbeniwal96@gmail.com>
2023-08-10 22:37:18 +0530

    Registraction form css

--- basics/RegistractionForm/SecondPage.js
+++ basics/RegistractionForm/SecondPage.js
@@ -1,6 +1,14 @@
 let submit = document.getElementById("btn");
 submit.addEventListener( "click",function(e){
     console.log("click called")
+    var genders = document.getElementById('gender').value;
+var rate_value;
+    if(genders == 'Fixed Rate'){
+        rate_value = document.getElementById('male').value;
+
+    }else if(rates == 'Variable Rate'){
+        rate_value = document.getElementById('female').value;
+
 
     let name = document.getElementById("FName").value;
     let name1 = document.getElementById("LName").value;
@@ -13,19 +21,26 @@ submit.addEventListener( "click",function(e){
     let emails = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     if(name==""){
-        alert("Name is empty")
+        document.querySelector(".FName-error").innerHTML = "Please enter a first name*";
+        // alert("Name is empty")
     }else if(name1==""){
-        alert("Last name is empty")
+        document.querySelector(".LName-error").innerHTML="Please enter last name*"
+        // alert("Last name is empty")
     }else if(father==""){
-        alert("Father's is empty")
+        document.querySelector(".FNames").innerHTML = "Please enter father's name*"
+        // alert("Father's is empty")
     }else if(mother==""){
-        alert("Mother's is empty")
+        document.querySelector('.mothers').innerHTML = "Please enter mother's name*"
+        // alert("Mother's is empty")
     }else if(Address==""){
-        alert("Address is empty")
+        document.querySelector('.address').innerHTML ="Please enter address*"
+        // alert("Address is empty")
     }else if(emails==""){
-        alert("Email id is empty")
+        document.querySelector('.email').innerHTML = "Please enter email id*"
+        // alert("Email id is empty")
     }else if(password==""){
-        alert("Password is empty")
+        document.querySelector('.password').innerHTML = "Please enter password*"
+        // alert("Password is empty")
     }
       
     
