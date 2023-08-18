35e67c9564a1802699de02c32a864d60baed2437
Anup <anupbeniwal96@gmail.com>
2023-08-18 16:45:41 +0530

    Json

---
+++ basics/Json/Json.js
@@ -0,0 +1,39 @@
+// // import data from './data.json';
+// // console.log(data);
+
+import data from './data.json' assert { type: 'json' };
+// // let a= data.id ;
+// // let b= data.Name;
+// // let c = data.Address;
+// // let key = Object.keys(data);
+// // console.log(key);
+// // const obj = JSON.stringify(data);
+// // document.getElementById("demo1").innerHTML =a;
+// // document.getElementById("demo2").innerHTML =b;
+// // document.getElementById("demo3").innerHTML =c;
+// var obj= data.em[0];
+// var a = obj.id;
+// var b = obj.Name;
+// var c = obj.Address;
+// console.log(a,b,c);
+// document.getElementById("demo1").innerHTML =a;
+// document.getElementById("demo2").innerHTML =b;
+// document.getElementById("demo3").innerHTML =c;
+// var x = obj.details.roll_no;
+// console.log("roll_no"+ x);
+// var obj1 = data.em[1];
+// var y=obj1.Address;
+// console.log("address"+ obj1.Address);
+// document.getElementById("demo1").innerHTML +=y;
+// var obj2 = data.em[2];
+// console.log("name"+" "+obj2.Name);
+
+// var ShareInfoLength = data.em.length;
+// console.log(ShareInfoLength);
+// for(var i=0; i<ShareInfoLength; i++)
+// {
+// console.log((data.em[i]));
+// }
+
+
+
