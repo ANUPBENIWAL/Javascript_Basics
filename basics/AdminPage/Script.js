e7b9de72afa715b495f27657b8644d6f41b0beb3
Anup <anupbeniwal96@gmail.com>
2023-08-26 22:25:18 +0530

    Develop in Admin page

--- basics/AdminPage/Script.js
+++ basics/AdminPage/Script.js
@@ -1,8 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function load_home() {
+  document.getElementById("mid").innerHTML='<object type="text/html" data="FirstPage.html" ></object>';
+}
