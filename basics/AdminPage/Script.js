73962a497261985ec8aa25eaba992a0657c95f06
Anup <anupbeniwal96@gmail.com>
2023-09-01 22:20:02 +0530

    Second page

--- basics/AdminPage/Script.js
+++ basics/AdminPage/Script.js
@@ -9,3 +9,7 @@ function closeNav() {
 function load_home() {
   document.getElementById("mid").innerHTML='<object type="text/html" data="FirstPage.html" ></object>';
 }
+
+function load_home1() {
+  document.getElementById("mid").innerHTML='<object type="text/html" data="Second.html" ></object>';
+}
